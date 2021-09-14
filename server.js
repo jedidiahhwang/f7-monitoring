832c0fdc8fc5af455f3fdebb7a9003a0334f5f35
Jeddy Hwang <jedidiah.hwang@gmail.com>
2021-09-14 09:22:01 -0600

    initialized rollbar

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const path = require("path");
+const Rollbar = require("rollbar");
+
+let rollbar = new Rollbar({
+    accesstoken: "a03378dc88e6432cb951b8dcad173608",
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
+    rollbar.info("HTML file served successfully!");
 })
 
 const port = process.env.PORT || 4545;
