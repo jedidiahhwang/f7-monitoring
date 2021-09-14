9b035ff0e722418b59d8a94d436218e897129800
Jeddy Hwang <jedidiah.hwang@gmail.com>
2021-09-14 09:39:28 -0600

    updated accessToken

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const Rollbar = require("rollbar");
 
 let rollbar = new Rollbar({
-    accesstoken: "a03378dc88e6432cb951b8dcad173608",
+    accessToken: "a03378dc88e6432cb951b8dcad173608",
     captureUncaught: true,
     captureUnhandledRejections: true
 })
